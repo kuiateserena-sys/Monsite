b49d4de5373870d2cf1bfcd55cbd2ab2d912d8e4
tsagueyves21 <tsagueyvesthibaut@gmail.com>
2026-08-03 23:31:17 -0400

    Add calculator functionality with UI and JavaScript logic

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var ourPurposeElement1 = document.querySelector('p');
 var p2 = document.querySelector('#serena');
 var box1 = document.querySelectorAll('.box1');
 
-console.log("box1:=>", box1[1]);
+// console.log("box1:=>", box1[1]);
 
 
 ourPurposeElement1.style.color = "red";
@@ -15,7 +15,7 @@ ourPurposeElement1.innerHTML = "Hello, this is a new text added to the paragraph
 
 
 // console.log("ourPurposeElement:=>", ourPurposeElement);
-console.log("ourPurposeElement1:=>", ourPurposeElement1);
+// console.log("ourPurposeElement1:=>", ourPurposeElement1);
 
 p2.addEventListener('click', () => {
     p2.classList.add('blue-background', 'text-white');
@@ -29,3 +29,33 @@ p2.addEventListener('mouseover', () => {
     box1[1].classList.remove('hidden-box');
 });
 
+
+// calculator
+const buttons = document.querySelectorAll('.buttons button');
+const resultScreen = document.getElementById('result-screen');
+
+console.log("buttons:=>", buttons);
+
+
+function appendToResult(value) {
+    console.log("selected-value:=>", value);
+    resultScreen.innerHTML += value;
+    
+}
+
+function clearResult() {
+    resultScreen.innerHTML = '';
+}
+
+function calculateResult() {
+    try {
+        console.log("resultScreen.innerHTML:=>", resultScreen.innerHTML);
+        
+        const result = eval(resultScreen.innerHTML);
+        resultScreen.innerHTML = result;
+    } catch (error) {
+        resultScreen.innerHTML = 'Erreur de syntaxe';
+        resultScreen.style.color = 'red';
+        resultScreen.style.border = '1px solid red';
+    }
+}
